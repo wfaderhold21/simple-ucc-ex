94ee1a581ea0ead0a5bb37239269996dcc4155d7
ferrol aderholdt <faderholdt@nvidia.com>
2025-02-25 22:35:47 -0800

    update host-to-host code

--- host-to-host/simple.c
+++ host-to-host/simple.c
@@ -11,13 +11,6 @@
 #include <ucp/api/ucp.h>
 #include <assert.h>
 
-long pSyncRed1[_SHMEM_REDUCE_SYNC_SIZE];
-long pSyncRed2[_SHMEM_REDUCE_SYNC_SIZE];
-long pSync_a2a[_SHMEM_ALLTOALL_SYNC_SIZE];
-
-double pWrk1[_SHMEM_REDUCE_MIN_WRKDATA_SIZE];
-double pWrk2[_SHMEM_REDUCE_MIN_WRKDATA_SIZE];
-
 void *a2a_psync;
 
 static ucc_status_t oob_allgather(void *sbuf, void *rbuf, size_t msglen,
@@ -28,7 +21,8 @@ static ucc_status_t oob_allgather(void *sbuf, void *rbuf, size_t msglen,
     MPI_Iallgather(sbuf, msglen, MPI_BYTE, rbuf, msglen, MPI_BYTE, comm,
                    &request);
     *req = calloc(1, sizeof(ucc_status_t));
-    /* FIXME: MPI_Test in oob_allgather_test results in no completion? leave as blocking for now */
+    /* FIXME: MPI_Test in oob_allgather_test results in no completion?
+     *        leave as blocking for now */
     MPI_Wait(&request, MPI_STATUS_IGNORE);
     *req = UCC_OK;
     return UCC_OK;
@@ -48,51 +42,50 @@ int setup_ucc(int rank, int size,
               ucc_lib_h *lib, 
               ucc_context_h *context, ucc_team_h *team)
 {
-    ucc_context_h ucc_context;
-    ucc_team_h  ucc_team;
-    ucc_lib_h ucc_lib;
-    ucc_team_params_t team_params = {0};
-    ucc_lib_config_h lib_config;
-    ucc_lib_params_t lib_params;
+    ucc_team_params_t    team_params = {0};
+    ucc_context_params_t ctx_params  = {0};
+    ucc_context_h        ucc_context;
+    ucc_team_h           ucc_team;
+    ucc_lib_h            ucc_lib;
+    ucc_lib_config_h     lib_config;
+    ucc_lib_params_t     lib_params;
     ucc_context_config_h ctx_config;
-    ucc_context_params_t ctx_params = {0};
-    ucc_status_t status;
-    ucc_mem_map_t map;
+    ucc_status_t         status;
+    ucc_mem_map_t        map;
 
     /* make ucc here */
-    lib_params.mask = UCC_LIB_PARAM_FIELD_THREAD_MODE;
+    lib_params.mask        = UCC_LIB_PARAM_FIELD_THREAD_MODE;
     lib_params.thread_mode = UCC_THREAD_SINGLE;
 
     if (UCC_OK != ucc_lib_config_read(NULL, NULL, &lib_config)) {
         printf("lib config error\n");
         return -1;
     }
-
     if (UCC_OK != ucc_init(&lib_params, lib_config, &ucc_lib)) {
         printf("lib init error\n");
         return -1;
     }
     *lib = ucc_lib;
 
-    a2a_psync = calloc(1,128);
+    a2a_psync   = calloc(1,128);
     map.address = a2a_psync;
-    map.len = 128;
+    map.len     = 128;
 
-    ctx_params.mask = UCC_CONTEXT_PARAM_FIELD_OOB | UCC_CONTEXT_PARAM_FIELD_MEM_PARAMS;
+    ctx_params.mask          = UCC_CONTEXT_PARAM_FIELD_OOB
+                             | UCC_CONTEXT_PARAM_FIELD_MEM_PARAMS;
     ctx_params.oob.allgather = oob_allgather;
     ctx_params.oob.req_test  = oob_allgather_test;
     ctx_params.oob.req_free  = oob_allgather_free;
     ctx_params.oob.coll_info = MPI_COMM_WORLD;
     ctx_params.oob.n_oob_eps = size;
     ctx_params.oob.oob_ep    = rank;
-    ctx_params.mem_params.segments = &map;
+    ctx_params.mem_params.segments   = &map;
     ctx_params.mem_params.n_segments = 1;
 
     if (UCC_OK != ucc_context_config_read(ucc_lib, NULL, &ctx_config)) {
         printf("error ucc ctx config read\n");
         return -1;
     }
-
     if (UCC_OK != ucc_context_create(ucc_lib, &ctx_params, ctx_config, &ucc_context)) {
         printf("ERROR ucc ctx create\n");
         return -1;
@@ -103,18 +96,17 @@ int setup_ucc(int rank, int size,
 
     team_params.mask = UCC_TEAM_PARAM_FIELD_EP | UCC_TEAM_PARAM_FIELD_OOB | UCC_TEAM_PARAM_FIELD_FLAGS;
     team_params.oob.allgather = oob_allgather;
-    team_params.oob.req_test = oob_allgather_test;
-    team_params.oob.req_free = oob_allgather_free;
+    team_params.oob.req_test  = oob_allgather_test;
+    team_params.oob.req_free  = oob_allgather_free;
     team_params.oob.coll_info = MPI_COMM_WORLD;
     team_params.oob.n_oob_eps = size;
-    team_params.oob.oob_ep = rank;
-    team_params.ep = rank;
+    team_params.oob.oob_ep    = rank;
+    team_params.ep            = rank;
 
     if (UCC_OK != ucc_team_create_post(&ucc_context, 1, &team_params, &ucc_team)) {
         printf("team create post failed\n");
         return -1;
     }
-
     while (UCC_INPROGRESS == (status = ucc_team_create_test(ucc_team))) {}
     if (UCC_OK != status) {
         printf("team create failed\n");
@@ -128,7 +120,7 @@ int setup_ucc(int rank, int size,
 int main(int argc, char *argv[])
 {
     size_t               exchange_size = 0;
-    size_t               call_size = 0;
+    size_t               call_size     = 0;
     int                  numprocs;
     int                  me;
     char                *s_buf_heap;
@@ -152,8 +144,8 @@ int main(int argc, char *argv[])
     numprocs = shmem_n_pes();
 
     shmem_barrier_all();
-    s_buf_heap = shmem_malloc(1024);
-    r_buf_heap = shmem_malloc(1024);
+    s_buf_heap = malloc(1024); //shmem_malloc(1024);
+    r_buf_heap = malloc(1024); //shmem_malloc(1024);
 
     global_source = malloc(numprocs * sizeof(ucc_mem_map_mem_h));
     global_dest   = malloc(numprocs * sizeof(ucc_mem_map_mem_h));
@@ -172,7 +164,7 @@ int main(int argc, char *argv[])
 
     /* currently limited to 1 segment per map_param for export */
     map_params.n_segments = 1;
-    map_params.segments = &map[0];
+    map_params.segments   = &map[0];
     status = ucc_mem_map(ucc_context, UCC_MEM_MAP_EXPORT, &map_params, &exchange_size, &local[0]);
     if (status != UCC_OK) {
         abort();
@@ -238,7 +230,7 @@ int main(int argc, char *argv[])
         },
         .flags = UCC_COLL_ARGS_FLAG_MEM_MAPPED_BUFFERS,
         .global_work_buffer = a2a_psync, /* mapped via context */
-        .src_memh.global_memh = global_source, 
+        .src_memh.local_memh = global_source[me],
         .dst_memh.global_memh = global_dest,
     };
     status = ucc_collective_init(&a2a_coll, &req, ucc_team);
